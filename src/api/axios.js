f236eedd29e22cf92a34f5e3adf745400b2b1103
wiseinvoker <wiseinvoker@gmail.com>
2025-06-06 11:50:47 -0400

    chore: replace api url with env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',  // Backend API URL
+  baseURL: import.meta.env.VITE_API_KEY,  // Backend API URL
   timeout: 5000,
 });
 
