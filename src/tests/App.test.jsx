87af1dd8639a9fa3c12e563124f981ba37c0611f
wiseinvoker <wiseinvoker@gmail.com>
2025-06-09 19:10:46 -0400

    test: remove unnecessary test cases

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders new slide link', () => {
+test('renders header title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/new slide/i);
-  expect(linkElement).toBeTruthy();
+  const headerElement = screen.getByText(/MarkdownPPT/i);
+  expect(headerElement).toBeTruthy();
 });
