3b7991858a0d6e7351f42bcc5caa39514108fa85
wiseinvoker <wiseinvoker@gmail.com>
2025-06-10 10:57:35 -0400

    chore: add nodePolyfils for buffer issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills],
   test: {
     globals: true,
     environment: 'jsdom'
